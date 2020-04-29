02e4d2077819bbd7007bfba851874e531632406a
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-04-29 22:32:34 +0900

    [ADD] RxJava Chapter 2-1

---
+++ RxJava-Learn/src/main/java/chapter2/FirstExample.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+import io.reactivex.Observable;
+
+public class FirstExample {
+	
+	public void emit() {
+		Observable.just(1, 2, 3, 4, 5, 6)
+		.subscribe(System.out::println);
+	}
+
+	public static void main(String[] args) {
+		FirstExample demo = new FirstExample();
+		demo.emit();
+
+	}
+
+}
