1febe22697923208952c8ab0a7719c340e94b634
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-06 22:43:06 +0900

    [ADD] RxJava Chapter 2-6

--- RxJava-Learn/src/main/java/chapter2/ObservableCreateExample.java
+++ RxJava-Learn/src/main/java/chapter2/ObservableCreateExample.java
@@ -2,6 +2,7 @@ package chapter2;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
+import io.reactivex.functions.Consumer;
 
 public class ObservableCreateExample {
 	
@@ -27,7 +28,7 @@ public class ObservableCreateExample {
 					emitter.onNext(300);
 					emitter.onComplete();
 				});
-		*/
+		
 		
 		//람다를 사용한 함수
 		Observable<Integer> source = Observable.create(
@@ -38,6 +39,23 @@ public class ObservableCreateExample {
 					emitter.onComplete();
 				});
 		source.subscribe(data -> System.out.println("Result : "+ data));
+		*/
+		
+		//익명 객체 활용 예
+		Observable<Integer> source = Observable.create(
+				(ObservableEmitter<Integer> emitter)-> {
+					emitter.onNext(100);
+					emitter.onNext(200);
+					emitter.onNext(300);
+					emitter.onComplete();
+				});
+		
+		source.subscribe(new Consumer<Integer>() {
+			@Override
+			public void accept(Integer data) throws Exception {
+				System.out.println("Result : "+ data);
+			}
+		});
 		
 	}
 
