9f47ab07a9db1a73a22ec216f2e83fbe9a25c39c
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-09 23:18:08 +0900

    [ADD] RxJava Chapter 2-8

--- RxJava-Learn/src/main/java/chapter2/ObservableFromArray.java
+++ RxJava-Learn/src/main/java/chapter2/ObservableFromArray.java
@@ -1,14 +1,26 @@
 package chapter2;
 
+import java.util.stream.IntStream;
+
 import io.reactivex.Observable;
 
 public class ObservableFromArray {
 
 	public static void main(String[] args) {
+		/*
 		Integer[] arr = {100, 200, 300};
 		Observable<Integer> source = Observable.fromArray(arr);
 		source.subscribe(System.out::println);
-
+		*/
+		
+		//int[] 사용했을 때
+		Integer[] intArray = {100, 200, 300};
+		Observable.fromArray(intArray).subscribe(System.out::println);
+		
+	}
+	
+	private static Integer[] toIntegerArray(int[] intArray) {
+		return IntStream.of(intArray).boxed().toArray(Integer[]::new);
 	}
 
 }
