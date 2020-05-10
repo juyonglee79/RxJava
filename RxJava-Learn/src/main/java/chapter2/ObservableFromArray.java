15e5d39ea16eb1c48d1b6867d81083dd69982fb6
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-10 22:54:48 +0900

    [ADD] RxJava Chapter 2-9

--- RxJava-Learn/src/main/java/chapter2/ObservableFromArray.java
+++ RxJava-Learn/src/main/java/chapter2/ObservableFromArray.java
@@ -11,12 +11,16 @@ public class ObservableFromArray {
 		Integer[] arr = {100, 200, 300};
 		Observable<Integer> source = Observable.fromArray(arr);
 		source.subscribe(System.out::println);
-		*/
+	
 		
 		//int[] 사용했을 때
 		Integer[] intArray = {100, 200, 300};
 		Observable.fromArray(intArray).subscribe(System.out::println);
+		*/
 		
+		int[] intArray = {400,500,600};
+		Observable<Integer> source = Observable.fromArray(toIntegerArray(intArray));
+		source.subscribe(System.out::println);
 	}
 	
 	private static Integer[] toIntegerArray(int[] intArray) {
