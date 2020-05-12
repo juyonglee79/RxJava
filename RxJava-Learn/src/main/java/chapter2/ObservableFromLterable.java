d2dea287cb0a1b53826b28dd3f8985b6aa60b85f
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-12 22:40:23 +0900

    [ADD] RxJava Chapter 2-10

--- RxJava-Learn/src/main/java/chapter2/ObservableFromLterable.java
+++ RxJava-Learn/src/main/java/chapter2/ObservableFromLterable.java
@@ -1,19 +1,32 @@
 package chapter2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import io.reactivex.Observable;
 
 public class ObservableFromLterable {
 
 	public static void main(String[] args) {
+		/*
 		List<String> names = new ArrayList<>();
 		names.add("JERRY");
 		names.add("William");
 		names.add("Bob");
 		
 		Observable<String> source = Observable.fromIterable(names);
+		source.subscribe(System.out::println);
+		*/
+		
+		//Set »ç¿ë
+		Set<String> cities = new HashSet<>();
+		cities.add("Seoul");
+		cities.add("London");
+		cities.add("Paris");
+		
+		Observable<String> source = Observable.fromIterable(cities);
 		source.subscribe(System.out::println);
 
 	}
