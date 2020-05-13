e666a4bee80367c4b19b5fdb8ef55afb3ba11890
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-13 14:16:47 +0900

    [ADD] RxJava Chapter 2-12

--- RxJava-Learn/src/main/java/chapter2/ObservableFromLterable.java
+++ RxJava-Learn/src/main/java/chapter2/ObservableFromLterable.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import org.apache.commons.math3.geometry.partitioning.BSPTreeVisitor.Order;
 
 import io.reactivex.Observable;
 
@@ -18,7 +22,7 @@ public class ObservableFromLterable {
 		
 		Observable<String> source = Observable.fromIterable(names);
 		source.subscribe(System.out::println);
-		*/
+		
 		
 		//Set 사용
 		Set<String> cities = new HashSet<>();
@@ -28,7 +32,17 @@ public class ObservableFromLterable {
 		
 		Observable<String> source = Observable.fromIterable(cities);
 		source.subscribe(System.out::println);
+		*/
 
+		//fromIterable()에 BlockingQueue 사용
+		BlockingQueue<Order> orderQueue = new ArrayBlockingQueue<>(100);
+		orderQueue.add(new Order("ORD-1"));
+		orderQueue.add(new Order("ORD-2"));
+		orderQueue.add(new Order("ORD-3"));
+		
+		Observable<Order> source = Observable.fromIterable(orderQueue);
+		source.subscribe(order -> System.out.println(order.getId()));
+		
 	}
 
 }
