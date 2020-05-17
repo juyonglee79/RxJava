d8b6563701c2c192b2d95f8b8e2d53f7c2defca4
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-17 23:38:22 +0900

    [ADD] RxJava Chapter 2-18

--- RxJava-Learn/src/main/java/chapter2/ObservableFromPublisher.java
+++ RxJava-Learn/src/main/java/chapter2/ObservableFromPublisher.java
@@ -6,8 +6,9 @@ import io.reactivex.Observable;
 
 public class ObservableFromPublisher {
 
-	//람다를 활용한 fromPublisher() 함수
 	public static void main(String[] args) {
+		/*
+		//람다를 활용한 fromPublisher() 함수
 		Publisher<String> publisher = (Subscriber<? super String> s) -> {
 			s.onNext("Hello Observable.fromPublisher()");
 			s.onComplete();
@@ -15,7 +16,19 @@ public class ObservableFromPublisher {
 		
 		Observable<String> source = Observable.fromPublisher(publisher);
 		source.subscribe(System.out::println);
-
+		*/
+		
+		//람다를 사용하지 않은 fromPublisher() 함수
+		Publisher<String> publisher = new Publisher<String>() {
+			@Override
+			public void subscribe(Subscriber<? super String> s) {
+				s.onNext("Hello Observable.fromPublisher()");
+				s.onComplete();
+			}
+		};
+		
+		Observable<String> source = Observable.fromPublisher(publisher);
+		source.subscribe(System.out::println);
 	}
 
 }
