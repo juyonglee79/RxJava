908a7dc2ed9542370a456a9f290c765fd96a1b03
juyonglee79 <37585586+juyonglee79@users.noreply.github.com>
2020-05-21 23:02:59 +0900

    [ADD] RxJava Chapter 2-25

--- RxJava-Learn/src/main/java/chapter2/AsyncSubjectExample.java
+++ RxJava-Learn/src/main/java/chapter2/AsyncSubjectExample.java
@@ -6,6 +6,8 @@ import io.reactivex.subjects.AsyncSubject;
 public class AsyncSubjectExample {
 
 	public static void main(String[] args) {
+		
+	/*
 		// AsyncSubject 클래스 활용 예시
 		AsyncSubject<String> subject = AsyncSubject.create();
 		subject.subscribe(data -> System.out.println("Subscriber #1 => "+ data));
@@ -23,6 +25,19 @@ public class AsyncSubjectExample {
 		subject.subscribe(data -> System.out.println("Subscriber #1 => "+ data));
 		
 		source.subscribe(subject);
+	*/
+		
+		AsyncSubject<Integer> subject = AsyncSubject.create();
+		subject.onNext(10);
+		subject.onNext(11);
+		subject.subscribe(data -> System.out.println());
+		subject.onNext(12);
+		subject.onComplete();
+		subject.onNext(13);
+		subject.subscribe(data -> System.out.println("Subscriber #2 => "+ data));
+		subject.subscribe(data -> System.out.println("Subscriber #3 => "+ data));
+		
+		
 	}
 
 }
